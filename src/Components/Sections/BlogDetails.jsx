cb3a2ac5863812dd57464aed4f675fae590de462
rahulsreekumar81 <rahulsreekumar81@gmail.com>
2022-05-03 14:09:26 +0530

    blog details done

--- src/Components/Sections/BlogDetails.jsx
+++ src/Components/Sections/BlogDetails.jsx
@@ -1,8 +1,25 @@
 import { useParams } from "react-router-dom";
-
+import useFetch from "../../Hooks/useFetch";
 const BlogDetails = () => {
   const { id } = useParams();
-  return <div>BlogDetails-{id}</div>;
+  const {
+    data: blog,
+    error,
+    isLoading,
+  } = useFetch("http://localhost:8000/blogs/" + id);
+  return (
+    <div className="blog-details">
+      {isLoading && <span>Loading please wait...</span>}
+      {error && <span style={{ color: "Red" }}>{error}</span>}
+      {blog && (
+        <article>
+          <h2>{blog.title}</h2>
+          <p>{blog.author}</p>
+          <div>{blog.body}</div>
+        </article>
+      )}
+    </div>
+  );
 };
 
 export default BlogDetails;
